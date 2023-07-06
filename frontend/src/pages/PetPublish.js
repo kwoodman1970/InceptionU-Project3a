b618abea08f42be7aaabfe8cd8cea1bc3010f77f
Kevin Woodman <kwoodman1970@yahoo.ca>
2023-07-06 15:44:02 -0600

    Finished integrating PetRecords.js component into PetPublish.js page

--- frontend/src/pages/PetPublish.js
+++ frontend/src/pages/PetPublish.js
@@ -9,21 +9,19 @@ const PetPublish = () => {
 
   return (
     <>
-      <PetRecords petObjectId='64907d5cf089b3a0e8ea61a3' setCurrentTab={setCurrentTab} />
-
       <h1 className='home-middel-penel fw-bold'>pet <span>cards</span></h1>
 
       <div className='tabsRow'>
         <div name={`${currentTab === 0}`}>General</div>
-        <div name={`${false}`}>Records</div>
-        <div name={`${currentTab === 1}`}>Contacts</div>
-        <div name={`${currentTab === 2}`}>Pedigree</div>
+        <div name={`${currentTab === 1}`}>Records</div>
+        <div name={`${currentTab === 2}`}>Contacts</div>
+        <div name={`${currentTab === 3}`}>Pedigree</div>
       </div>
       <div style={{border: '1px solid black', padding: '4px'}}>
-        {currentTab === 0 && <PetGeneral cardType='Supplier' setCurrentTab={setCurrentTab} setPetObjectId={setPetObjectId} />}
-        {currentTab === -1 && <p>Records component goes here (Pet object ID is {petObjectId ? petObjectId : 'unknown'})</p>}
-        {currentTab === 1 && <p>Contacts component goes here (Pet object ID is {petObjectId ? petObjectId : 'unknown'})</p>}
-        {currentTab === 2 && <p>Pedigree component goes here (Pet object ID is {petObjectId ? petObjectId : 'unknown'})</p>}
+        {currentTab === 0 && <PetGeneral setCurrentTab={setCurrentTab} setPetObjectId={setPetObjectId} cardType='Supplier' />}
+        {currentTab === 1 && <PetRecords setCurrentTab={setCurrentTab} petObjectId={petObjectId} />}
+        {currentTab === 2 && <p>Contacts component goes here (Pet object ID is {petObjectId ? petObjectId : 'unknown'})</p>}
+        {currentTab === 3 && <p>Pedigree component goes here (Pet object ID is {petObjectId ? petObjectId : 'unknown'})</p>}
       </div>
     </>);
 }
